ed3130d0797e25a38d0180e1e5b75c86f591e043
smiiiiin <mimple1997@icloud.com>
2024-10-07 09:59:31 +0900

    n

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -1,17 +1,10 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by Uin on 2024/10/01.
-//
-
 import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct ScrumsView: View { // list 
+    let scrums: [DailyScrum]
+  
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
