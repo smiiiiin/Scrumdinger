02699b3f96cc010286e601327c4b8807889f816e
smiiiiin <mimple1997@icloud.com>
2024-10-31 23:59:25 +0900

    d

--- Scrumdinger/Views/ScrumProgressViewStyle.swift
+++ Scrumdinger/Views/ScrumProgressViewStyle.swift
@@ -6,15 +6,15 @@ struct ScrumProgressViewStyle: ProgressViewStyle {
     func makeBody(configuration: Configuration) -> some View {
         ZStack { // 겹쳐 쌓아서 여기 안에서 마지막에 쓴 코드가 제일 위에 오도록
             Circle()
-                .trim(from: 0, to: CGFloat(configuration.fractionCompleted ?? 0)) //
+                .trim(from: 0, to: CGFloat(configuration.fractionCompleted ?? 0))
                 .rotation(Angle(degrees: 270))
-                .stroke(Color.red, lineWidth: 100)
-                .frame(width: 100, height: 100) // 이게 뭐냐? 좀 작게 하고 싶은데
+                .stroke(Color.red, lineWidth: 30)
+                .frame(width: 30, height: 30)
                 .padding(.horizontal)
             
             Text("\(Int((configuration.fractionCompleted ?? 0) * 100))%")
                 .font(.headline)
-                .foregroundColor(Color.white)
+                .foregroundColor(theme.accentColor)
         }
     }
 }
